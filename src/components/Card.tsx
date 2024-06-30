e0a294ca0446a22611755fdae9477e3e7d11c762
Ninottt9 <mkazhaev746@gmail.com>
2024-06-30 18:34:14 +0200

    card styles improved

---
+++ src/components/Card.tsx
@@ -0,0 +1,20 @@
+// src/components/Card.js
+
+import React from 'react';
+
+const Card = ({ eye_color, gender, skin_color, mass, name, birth_year, hair_color, height }) => {
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-4 mb-4 w-full max-w-sm">
+      <h3 className="text-lg font-bold mb-2">{name}</h3>
+      <p className="text-gray-700 mb-2">Eye Color: {eye_color}</p>
+      <p className="text-gray-700 mb-2">Gender: {gender}</p>
+      <p className="text-gray-700 mb-2">Skin Color: {skin_color}</p>
+      <p className="text-gray-700 mb-2">Mass: {mass} kg</p>
+      <p className="text-gray-700 mb-2">Birth Year: {birth_year}</p>
+      <p className="text-gray-700 mb-2">Hair Color: {hair_color}</p>
+      <p className="text-gray-700 mb-2">Height: {height} cm</p>
+    </div>
+  );
+};
+
+export default Card;
--- src/components/Inventory.tsx
+++ src/components/Inventory.tsx
@@ -1,7 +1,10 @@
-import { useEffect, useState } from 'react';
-import api from '../services/api';
+// src/components/Inventory.js
 
-export default function Inventory() {
+import React, { useEffect, useState } from 'react';
+import api from '../services/api';
+import Card from './Card';
+
+const Inventory = () => {
   const [inventory, setInventory] = useState([]);
 
   const fetchInventory = async () => {
@@ -12,13 +15,14 @@ export default function Inventory() {
       console.error('Error fetching inventory:', error);
     }
   };
+
   useEffect(() => {
     fetchInventory();
   }, []);
 
-  const pushToInventory = async () => {
+  const openCreate = async () => {
     try {
-      const response = await api.post('/pushToInventory/');
+      await api.post('/pushToInventory/');
     } catch (error) {
       console.error('Error pushing to inventory:', error);
     }
@@ -26,15 +30,22 @@ export default function Inventory() {
   };
 
   return (
-    <div>
-      <h1>Inventory</h1>
-      <p>Welcome to the Inventory page!</p>
-      <button onClick={pushToInventory}>Push to Inventory</button>
-      <ul>
-        {inventory.map((name, id) => (
-          <li key={`${id}${name}`}>{name}</li>
+    <div className='flex flex-col items-center'>
+      <h1 className='text-4xl font-bold mb-4'>Inventory</h1>
+      <p className='text-lg mb-6'>Welcome to the Inventory page!</p>
+      <button
+        onClick={openCreate}
+        className='mb-6 bg-blue-500 text-white py-2 px-4 rounded-lg font-semibold hover:bg-blue-600 transition duration-200'
+      >
+        Open Create
+      </button>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+        {inventory.map((item, index) => (
+          <Card key={index} {...JSON.parse(item)} />
         ))}
-      </ul>
+      </div>
     </div>
   );
-}
+};
+
+export default Inventory;
